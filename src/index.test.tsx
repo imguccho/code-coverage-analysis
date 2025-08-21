50d0421d5d8ebc6bbcc5acf4a319154bd5ea5371
ambikarao <63193852+ambikarao@users.noreply.github.com>
2025-08-21 16:38:39 +0530

    Update test coverage for src/index.tsx

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,1 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+describe('Index File', () => {
+  test('renders App component without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('reportWebVitals function is called', () => {
+    const mockFn = jest.fn();
+    reportWebVitals(mockFn);
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
